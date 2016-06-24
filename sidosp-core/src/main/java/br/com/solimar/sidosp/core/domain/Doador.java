00536fe52773f68d2622289318ce38e5cae5fe70
solimarss <solimarss@rpawsgweb04.redetre.jus.br>
2016-06-24 09:55:31 -0300

    add entity Doador

---
+++ sidosp-core/src/main/java/br/com/solimar/sidosp/core/domain/Doador.java
@@ -0,0 +1,19 @@
+package br.com.solimar.sidosp.core.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Doador implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+}

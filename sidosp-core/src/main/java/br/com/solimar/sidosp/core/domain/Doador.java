c667ee405a05c46b578f7aa91911d05c8cca11d6
Solimar <Solimar@NotSol>
2016-06-24 22:11:03 -0300

    a

--- sidosp-core/src/main/java/br/com/solimar/sidosp/core/domain/Doador.java
+++ sidosp-core/src/main/java/br/com/solimar/sidosp/core/domain/Doador.java
@@ -2,6 +2,7 @@ package br.com.solimar.sidosp.core.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,5 +16,48 @@ public class Doador implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String mail;
+	
+	@Column
+	private String password;
+	
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
 }

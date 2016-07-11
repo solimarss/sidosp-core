f5d28f5f60bf3a6b11c3ff1cafe17496bdd35ea0
solimarss <solimarss@rpawsgweb04.redetre.jus.br>
2016-07-11 11:51:03 -0300

    incluídos Exame e Coleta

---
+++ sidosp-core/src/main/java/br/com/solimar/sidosp/core/domain/Coleta.java
@@ -0,0 +1,104 @@
+package br.com.solimar.sidosp.core.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+@Entity
+public class Coleta implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private Long numero;
+	
+	@ManyToOne
+	private Doador doador;
+	
+	@ManyToOne
+	private Laboratorio laboratorio;
+	
+	@Temporal(value=TemporalType.DATE)
+	private Date data;
+	
+	@Temporal(value=TemporalType.TIME)
+	private Date horario;
+	
+	@OneToMany(mappedBy="coleta")
+	private List<Exame> exames;
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+
+	public Doador getDoador() {
+		return doador;
+	}
+
+	public void setDoador(Doador doador) {
+		this.doador = doador;
+	}
+
+	public Laboratorio getLaboratorio() {
+		return laboratorio;
+	}
+
+	public void setLaboratorio(Laboratorio laboratorio) {
+		this.laboratorio = laboratorio;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Date getHorario() {
+		return horario;
+	}
+
+	public void setHorario(Date horario) {
+		this.horario = horario;
+	}
+
+	public List<Exame> getExames() {
+		return exames;
+	}
+
+	public void setExames(List<Exame> exames) {
+		this.exames = exames;
+	}
+
+	
+
+	
+}
---
+++ sidosp-core/src/main/java/br/com/solimar/sidosp/core/domain/Exame.java
@@ -0,0 +1,64 @@
+package br.com.solimar.sidosp.core.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Exame implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	private Coleta coleta;
+	
+	@Column
+	private Long codigo;
+	
+	@Column
+	private Long nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Coleta getColeta() {
+		return coleta;
+	}
+
+	public void setColeta(Coleta coleta) {
+		this.coleta = coleta;
+	}
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public Long getNome() {
+		return nome;
+	}
+
+	public void setNome(Long nome) {
+		this.nome = nome;
+	}
+	
+
+	
+}
